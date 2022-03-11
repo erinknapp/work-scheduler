4c05608b8d1161051befe8cee055d857adf6f0ad
Erin Knapp <erin.knapp@gmail.com>
2022-03-11 13:35:46 -0500

    btn clicks work

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-
 // Using moment.js to help format and display current time
 $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm a'));
 
@@ -15,4 +14,25 @@ $('.description').each(function(){
         $(this).addClass('present');
     }
 });
+
+if (moment().isAfter(currentTime)){
+    // add class for tasks that are nearly due
+    // add class for tasks that have a lot of time before due
+}
+
+// logic for button clicks
+$('.saveBtn').on("click", function(){
+    var thisBtn = $(this);
+    var mainDiv = thisBtn.parent();
+    var textarea = mainDiv.find("textarea");
+    var text = textarea.val();
+    var key = textarea.attr("id");
+    localStorage.setItem(key, text);
+    console.log(text);
+})
+
+for(var hour = 9; hour<18; hour++) {
+    $("#" + hour).val(localStorage.getItem(hour));
+}
+
     
